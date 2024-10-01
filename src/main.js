943274bc1bd1ea33efd6bf3053c6ce1a409977db
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-10-01 11:02:09 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
-import HeaderComponent from '../src/view/header-component.js';
-import FormAddTaskComponent from '../src/view/form-add-task-component.js';
-import TaskBarComponent from '../src/view/taskbar-component.js';
-import ListTaskComponent from '../src/view/list-task-component.js';
-import TaskComponent from '../src/view/task-component.js';
+import HeaderComponent from './view/header-component.js';
+import FormAddTaskComponent from './view/form-add-task-component.js';
+import TaskBarComponent from './view/taskbar-component.js';
 import {render, RenderPosition} from './framework/render.js';
-
 
 const bodyContainer = document.querySelector('.board-app');
 const formContainer = document.querySelector('.add-task');
-const barcontainer = document.querySelector('.taskboard');
-const listcontainer = document.querySelector('.taskboard');
-const taskcontainer = document.querySelector('.taskboard');
+const tasksBoardContainer = document.querySelector('.taskboard');
+
+// Пример данных задач с русскими названиями статусов
+const tasksByStatus = {
+  'К выполнению': ['Задача 1', 'Задача 2', 'Задача 3'],
+  'В процессе': ['Задача 4', 'Задача 5', 'Задача 6'],
+  'Готово': ['Задача 7', 'Задача 8', 'Задача 9'],
+  'Корзина': ['Задача 10', 'Задача 11', 'Задача 12']
+};
 
 
 render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
 render(new FormAddTaskComponent(), formContainer);
-render(new TaskBarComponent(), barcontainer);
-render(new ListTaskComponent(), listcontainer);
-render(new TaskComponent(), taskcontainer);
+render(new TaskBarComponent(tasksByStatus), tasksBoardContainer);
