7bc0e669d081196da262748e0c4dbe4bca1da0f6
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-10-01 11:04:09 +0300

    Update list-task-component.js

--- src/view/list-task-component.js
+++ src/view/list-task-component.js
@@ -1,28 +1,42 @@
 import {createElement} from '../framework/render.js';
+import TaskComponent from './task-component.js';
 
-
-function createListTaskComponentTemplate() {
-    return (
-        ``
-      );
+function createListTaskComponentTemplate(title, className) {
+    return `
+      <div class="task-block ${className}">
+        <h3>${title}</h3>
+        <ul class="task-list"></ul>
+      </div>
+    `;
 }
 
-
 export default class ListTaskComponent {
-  getTemplate() {
-    return createListTaskComponentTemplate();
+  constructor(title, tasks, className) {
+    this.title = title;
+    this.tasks = tasks;
+    this.className = className; // добавляем класс для колонки
   }
 
+  getTemplate() {
+    return createListTaskComponentTemplate(this.title, this.className);
+  }
 
   getElement() {
     if (!this.element) {
       this.element = createElement(this.getTemplate());
+      this.renderTasks();
     }
-
 
     return this.element;
   }
 
+  renderTasks() {
+    const taskListElement = this.element.querySelector('.task-list');
+    this.tasks.forEach(task => {
+      const taskComponent = new TaskComponent(task);
+      taskListElement.appendChild(taskComponent.getElement());
+    });
+  }
 
   removeElement() {
     this.element = null;
