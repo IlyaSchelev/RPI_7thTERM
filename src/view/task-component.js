c46f0094f08fd4665b845205ca565804ffe14ba8
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-10-01 11:04:25 +0300

    Update task-component.js

--- src/view/task-component.js
+++ src/view/task-component.js
@@ -1,28 +1,25 @@
 import {createElement} from '../framework/render.js';
 
-
-function createTaskComponentTemplate() {
-    return (
-        ``
-      );
+function createTaskComponentTemplate(taskName) {
+    return `<li class="task-item">${taskName}</li>`;
 }
 
-
 export default class TaskComponent {
-  getTemplate() {
-    return createTaskComponentTemplate();
+  constructor(taskName) {
+    this.taskName = taskName;
   }
 
+  getTemplate() {
+    return createTaskComponentTemplate(this.taskName);
+  }
 
   getElement() {
     if (!this.element) {
       this.element = createElement(this.getTemplate());
     }
 
-
     return this.element;
   }
-
 
   removeElement() {
     this.element = null;
