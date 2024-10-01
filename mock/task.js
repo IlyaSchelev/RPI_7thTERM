a762fc5906f748cf145ae550fe0058fc798540ae
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-10-01 11:26:50 +0300

    upload mock, model, presenter and const.js

---
+++ mock/task.js
@@ -0,0 +1,55 @@
+export const tasks = [
+    {
+        'id': '1',
+        'title': 'Выучить js',
+        'status': 'backlog',
+    },
+
+    {
+       'id': '2',
+        'title': 'Попить',
+        'status': 'backlog', 
+    },
+
+    {
+        'id': '3',
+        'title': 'Выучить js',
+        'status': 'backlog',
+    },
+
+    {
+       'id': '1',
+        'title': 'Выучить js',
+        'status': 'backlog', 
+    },
+
+    {
+        'id': '2',
+        'title': 'Выучить js',
+        'status': 'backlog',
+    },
+
+    {
+       'id': '3',
+        'title': 'Выучить js',
+        'status': 'backlog', 
+    },
+
+    {
+        'id': '1',
+        'title': 'Выучить js',
+        'status': 'backlog',
+    },
+
+    {
+       'id': '2',
+        'title': 'Выучить js',
+        'status': 'backlog', 
+    },
+
+    {
+        'id': '3',
+         'title': 'Выучить js',
+         'status': 'backlog', 
+    },
+]
