8abed7a7e3ef6e6a3e3e91c7842d895c76f51cf0
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-10-15 19:34:17 +0300

    Update tasks-board-presenter.js

--- presenter/tasks-board-presenter.js
+++ presenter/tasks-board-presenter.js
@@ -1,33 +1,37 @@
 import TasksListComponent from '../view/list-task-component.js';
 import TaskComponent from '../view/task-component.js';
 import TaskBoardComponent from '../view/taskbar-component.js';
+import { Status, StatusLabel} from '../const.js';
 import {render} from '../framework/render.js';
 
-
 export default class TasksBoardPresenter {
- tasksBoardComponent = new TaskBoardComponent()
- taskListComponent = new TasksListComponent();
+    tasksBoardComponent = new TaskBoardComponent();
 
+    constructor({boardContainer, tasksModel}) {
+        this.boardContainer = boardContainer;
+        this.tasksModel = tasksModel;
+    }
 
- constructor({boardContainer, tasksModel}) {
-   this.boardContainer = boardContainer;
-   this.tasksModel = tasksModel;
- }
+    init() {
+      this.boardTasks = [...this.tasksModel.getTasks()];
 
+      render(this.tasksBoardComponent, this.boardContainer);
 
- init() {
-    this.boardTasks = [...this.tasksModel.getTasks()];
+      // Преобразуем объект Status в массив значений
+      Object.values(Status).forEach((status) => {
+          // Передаем заголовок из StatusLabel для каждого статуса
+          const tasksListComponent = new TasksListComponent({ 
+              status, 
+              title: StatusLabel[status]  // Используем соответствующий заголовок для каждого статуса
+          });
+          render(tasksListComponent, this.tasksBoardComponent.getElement());
 
-   render(this.tasksBoardComponent, this.boardContainer);
-   for (let i = 0; i < 4; i++) {
-       const tasksListComponent = new TasksListComponent();
-       render(tasksListComponent, this.tasksBoardComponent.getElement());
-      
-       for (let j = 0; j < 4; j++) {
-           const taskComponent = new TaskComponent();
-           render(taskComponent, tasksListComponent.getElement());
-       }
-   }
- 
- }
+          const tasksByStatus = this.boardTasks.filter(task => task.status === status);
+
+          tasksByStatus.forEach(task => {
+              const taskComponent = new TaskComponent({ task });
+              render(taskComponent, tasksListComponent.getElement().querySelector('.task-list'));
+          });
+      });
+  }
 }
