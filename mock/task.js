1713e7be60815f44b01858d6990277a5755c0384
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-10-15 19:36:03 +0300

    Update task.js

--- mock/task.js
+++ mock/task.js
@@ -1,55 +1,55 @@
 export const tasks = [
     {
         'id': '1',
-        'title': 'Выучить js',
+        'title': 'Задача 1',
         'status': 'backlog',
-    },
-
-    {
-       'id': '2',
-        'title': 'Попить',
-        'status': 'backlog', 
-    },
-
-    {
-        'id': '3',
-        'title': 'Выучить js',
-        'status': 'backlog',
-    },
-
-    {
-       'id': '1',
-        'title': 'Выучить js',
-        'status': 'backlog', 
     },
 
     {
         'id': '2',
-        'title': 'Выучить js',
-        'status': 'backlog',
-    },
-
-    {
-       'id': '3',
-        'title': 'Выучить js',
-        'status': 'backlog', 
-    },
-
-    {
-        'id': '1',
-        'title': 'Выучить js',
-        'status': 'backlog',
-    },
-
-    {
-       'id': '2',
-        'title': 'Выучить js',
-        'status': 'backlog', 
+        'title': 'Задача 2',
+        'status': 'in-progress', 
     },
 
     {
         'id': '3',
-         'title': 'Выучить js',
+        'title': 'Задача 3',
+        'status': 'trash',
+    },
+
+    {
+       'id': '4',
+        'title': 'Задача 4',
+        'status': 'backlog', 
+    },
+
+    {
+        'id': '5',
+        'title': 'Задача 5',
+        'status': 'done',
+    },
+
+    {
+       'id': '6',
+        'title': 'Задача 6',
+        'status': 'done', 
+    },
+
+    {
+        'id': '7',
+        'title': 'Задача 7',
+        'status': 'trash',
+    },
+
+    {
+       'id': '8',
+        'title': 'Задача 8',
+        'status': 'done', 
+    },
+
+    {
+        'id': '',
+         'title': 'Задача 9',
          'status': 'backlog', 
     },
 ]
