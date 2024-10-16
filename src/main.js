a33bebf0ecba56ceb76a5723a6837028911239eb
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-10-16 15:06:51 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import ListTaskComponent from './view/list-task-component.js';
 import TaskComponent from './view/task-component.js';
 import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
 import TasksModel from './model/tasks-model.js';
+import {AbstractComponent} from './framework/view/abstract-component.js';
 import {render, RenderPosition} from './framework/render.js';
+import ClearButtonComponent from './view/clear-button-component.js';
 
 const bodyContainer = document.querySelector('.board-app');
 const formContainer = document.querySelector('.add-task');
@@ -19,7 +21,7 @@ const tasksBoardPresenter = new TasksBoardPresenter({
 render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
 render(new FormAddTaskComponent(), formContainer, RenderPosition.AFTERBEGIN);
 render(new TaskBarComponent(), tasksBoardContainer, RenderPosition.BEFOREBEGIN);
-render(tasksBoardPresenter.tasksBoardComponent, tasksBoardContainer, RenderPosition.BEFOREBEGIN);
-
+// render(tasksBoardPresenter.tasksBoardComponent, tasksBoardContainer, RenderPosition.BEFOREBEGIN);
+render(new ClearButtonComponent(), tasksBoardContainer, RenderPosition.AFTEREND);
 
 tasksBoardPresenter.init();
