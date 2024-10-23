3cf122c2e6a2da98809f15b0d169945b34139fd7
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-10-23 13:51:17 +0300

    Update tasks-board-presenter.js
    creating a stub for empty tasks

--- presenter/tasks-board-presenter.js
+++ presenter/tasks-board-presenter.js
@@ -1,37 +1,47 @@
 import TasksListComponent from '../view/list-task-component.js';
 import TaskComponent from '../view/task-component.js';
 import TaskBoardComponent from '../view/taskbar-component.js';
-import { Status, StatusLabel} from '../const.js';
-import {render} from '../framework/render.js';
+import { Status, StatusLabel } from '../const.js';
+import { render } from '../framework/render.js';
 
 export default class TasksBoardPresenter {
-    tasksBoardComponent = new TaskBoardComponent();
+    #boardContainer;
+    #tasksModel;
+    #boardTasks;
+    #tasksBoardComponent = new TaskBoardComponent();
 
-    constructor({boardContainer, tasksModel}) {
-        this.boardContainer = boardContainer;
-        this.tasksModel = tasksModel;
+    constructor({ boardContainer, tasksModel }) {
+        this.#boardContainer = boardContainer;
+        this.#tasksModel = tasksModel;
     }
 
     init() {
-      this.boardTasks = [...this.tasksModel.getTasks()];
+        this.#boardTasks = [...this.#tasksModel.getTasks()];
 
-      render(this.tasksBoardComponent, this.boardContainer);
+        render(this.#tasksBoardComponent, this.#boardContainer);
 
-      // Преобразуем объект Status в массив значений
-      Object.values(Status).forEach((status) => {
-          // Передаем заголовок из StatusLabel для каждого статуса
-          const tasksListComponent = new TasksListComponent({ 
-              status, 
-              title: StatusLabel[status]  // Используем соответствующий заголовок для каждого статуса
-          });
-          render(tasksListComponent, this.tasksBoardComponent.getElement());
+        Object.values(Status).forEach((status) => {
+            const tasksByStatus = this.#boardTasks.filter(task => task.status === status);
+            const hasTasks = tasksByStatus.length > 0;  // Проверка наличия задач для статуса
 
-          const tasksByStatus = this.boardTasks.filter(task => task.status === status);
+            const tasksListComponent = new TasksListComponent({
+                status,
+                title: StatusLabel[status],  // Заголовок для каждого статуса
+                hasTasks  // Передаем флаг наличия задач
+            });
 
-          tasksByStatus.forEach(task => {
-              const taskComponent = new TaskComponent({ task });
-              render(taskComponent, tasksListComponent.getElement().querySelector('.task-list'));
-          });
-      });
-  }
+            render(tasksListComponent, this.#tasksBoardComponent.element);
+
+            if (hasTasks) {
+                tasksByStatus.forEach(task => {
+                    this.#renderTask(task, tasksListComponent.element.querySelector('.task-list'));
+                });
+            }
+        });
+    }
+
+    #renderTask(task, container) {
+        const taskComponent = new TaskComponent({ task });
+        render(taskComponent, container);
+    }
 }
