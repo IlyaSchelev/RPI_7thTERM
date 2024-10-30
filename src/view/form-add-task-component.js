541fdef9c02b418d52cda9d3f57b16b86ae0f39a
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-10-30 20:37:23 +0300

    Update form-add-task-component.js

--- src/view/form-add-task-component.js
+++ src/view/form-add-task-component.js
@@ -1,34 +1,41 @@
-import {createElement} from '../framework/render.js';
-import {AbstractComponent} from '../framework/view/abstract-component.js';
-
+import { createElement } from '../framework/render.js';
+import { AbstractComponent } from '../framework/view/abstract-component.js';
 
 function createFormAddTaskComponentTemplate() {
     return (
         `<section class="new-task">
             <h3>Новая задача</h3>
-            <input type="text" placeholder="Название задачи..." />
-            <button>+ Добавить</button>
+            <input type="text" class="task-input" placeholder="Название задачи..." />
+            <button type="button" class="add-task-button">+ Добавить</button>
         </section>`
-      );
+    );
 }
 
-
 export default class FormAddTaskComponent extends AbstractComponent {
+
+  #handleClick = null;
+
+  constructor({ onClick }) {
+    super();
+    this.#handleClick = onClick;
+
+    this.element.querySelector('.add-task-button').addEventListener('click', this.#clickHandler);
+  }
+
   get template() {
     return createFormAddTaskComponentTemplate();
   }
 
-  // getElement() {
-  //   if (!this.element) {
-  //     this.element = createElement(this.getTemplate());
-  //   }
+  #clickHandler = (evt) => {
+    evt.preventDefault();
 
-
-  //   return this.element;
-  // }
-
-
-  // removeElement() {
-  //   this.element = null;
-  // }
+    const taskInputValue = this.element.querySelector('.task-input').value.trim();
+    
+    if (taskInputValue) {
+      this.#handleClick(taskInputValue);  
+      this.element.querySelector('.task-input').value = '';  
+    } else {
+      alert('Введите название задачи');
+    }
+  }
 }
