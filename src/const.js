fa34ef9eca63d31a2bc7821ba8886aca4c1d8fa2
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-10-30 21:12:19 +0300

    upload new changed files
    folders mock, model, presenter, const.js and utils.js

---
+++ src/const.js
@@ -0,0 +1,15 @@
+export const Status = {
+  BACKLOG: `backlog`,
+  INPROGRESS: `in-progress`,
+  DONE: `done`,
+  TRASH: `trash`,
+};
+
+
+
+export const StatusLabel = {
+  [Status.BACKLOG]: `Бэклог`,
+  [Status.INPROGRESS]: `В процессе`,
+  [Status.DONE]: `Готово`,
+  [Status.TRASH]: `Корзина`,
+};
