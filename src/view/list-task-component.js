3e3f1d79e1b7ca9e5e8add9141106acb0bfc01d4
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-10-30 20:35:30 +0300

    Update list-task-component.js

--- src/view/list-task-component.js
+++ src/view/list-task-component.js
@@ -12,14 +12,31 @@ function createListTaskComponentTemplate(status, title, hasTasks) {
 }
 
 export default class ListTaskComponent extends AbstractComponent {
-    constructor({ status, title, hasTasks }) {
+    constructor({ status, title, hasTasks, onTaskDrop }) {
         super();
         this.status = status;
         this.title = title;
-        this.hasTasks = hasTasks;  // Флаг для контроля наличия задач
+        this.hasTasks = hasTasks;
+        this.#setDropHandler(onTaskDrop);
     }
 
     get template() {
         return createListTaskComponentTemplate(this.status, this.title, this.hasTasks);
     }
+
+    #setDropHandler(onTaskDrop) {
+        const container = this.element;
+
+        container.addEventListener('dragover', (event) => {
+            event.preventDefault();
+        });
+
+        container.addEventListener('drop', (event) => {
+            event.preventDefault();
+            const taskId = event.dataTransfer.getData('text/plain');
+            onTaskDrop(taskId, this.status);
+        });
+
+    }
+
 }
