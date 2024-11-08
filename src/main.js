420cdd6edac0522d1ebf05289057a2e1c29036ac
IlyaSchelev <160210145+IlyaSchelev@users.noreply.github.com>
2024-11-08 22:11:01 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,16 +3,20 @@ import FormAddTaskComponent from './view/form-add-task-component.js';
 import TaskBarComponent from './view/taskbar-component.js';
 import ListTaskComponent from './view/list-task-component.js';
 import TaskComponent from './view/task-component.js';
+import ClearButtonComponent from './view/clear-button-component.js';
 import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
 import TasksModel from './model/tasks-model.js';
 import {AbstractComponent} from './framework/view/abstract-component.js';
+import TasksApiService from './tasks-api-service.js';
 import {render, RenderPosition} from './framework/render.js';
-import ClearButtonComponent from './view/clear-button-component.js';
 
+const END_POINT = 'https://67226b092108960b9cc45a64.mockapi.io';
 const bodyContainer = document.querySelector('.board-app');
 const formContainer = document.querySelector('.add-task');
 const tasksBoardContainer = document.querySelector('.taskboard');
-const tasksModel = new TasksModel();
+const tasksModel = new TasksModel({
+    tasksApiService: new TasksApiService(END_POINT)
+})
 const tasksBoardPresenter = new TasksBoardPresenter({
  boardContainer: tasksBoardContainer, tasksModel,
 });
@@ -21,14 +25,15 @@ const formAddTaskComponent = new FormAddTaskComponent({
 });
 
 function handleNewTaskButtonClick(taskTitle) {
-    tasksBoardPresenter.createTask(taskTitle);  // Передаем taskTitle
+    tasksBoardPresenter.createTask(taskTitle);  
 }
+
 
 
 
 render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
 render(formAddTaskComponent, formContainer, RenderPosition.AFTERBEGIN);
 render(new TaskBarComponent(), tasksBoardContainer, RenderPosition.BEFOREBEGIN);
-render(new ClearButtonComponent(), tasksBoardContainer, RenderPosition.AFTEREND);
 
-tasksBoardPresenter.init();
+await tasksModel.init();
+await tasksBoardPresenter.init();
